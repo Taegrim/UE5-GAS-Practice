5d5e37eebd8fa8cad4fb59477fff90039c26a162
LeeDongHyun <aopo3948@gmail.com>
2026-06-08 17:47:10 +0900

    게임모드, 플레이어 캐릭터, IMC, IA 설정
    - GameModeBase를 상속받아서 생성
    - 플레이어 캐릭터 클래스 작성 및 카메라, 스프링암 부착, 기본 설정 작성
    - 플레이어 컨트롤러 클래스 작성
    - 플레이어 컨트롤러 내에서 IMC 추가, IA 함수 처리함

---
+++ Source/Crash/Private/Player/CC_PlayerController.cpp
@@ -0,0 +1,71 @@
+﻿#include "Crash/Public/Player/CC_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "GameFramework/Character.h"
+
+void ACC_PlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+    if (!IsValid(InputSubsystem)) return;
+
+    for (UInputMappingContext* Context : InputMappingContexts)
+    {
+        InputSubsystem->AddMappingContext(Context, 0);
+    }
+
+    UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+    if (!IsValid(EnhancedInputComponent)) return;
+
+    EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+    EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+    EnhancedInputComponent->BindAction(PrimaryAction, ETriggerEvent::Started, this, &ThisClass::Primary);
+}
+
+void ACC_PlayerController::Jump()
+{
+    if (!IsValid(GetCharacter())) return;
+
+    GetCharacter()->Jump();
+}
+
+void ACC_PlayerController::StopJumping()
+{
+    if (!IsValid(GetCharacter())) return;
+
+    GetCharacter()->StopJumping();
+}
+
+void ACC_PlayerController::Move(const FInputActionValue& Value)
+{
+    if (!IsValid(GetCharacter())) return;
+
+    const FVector2D MovementVector = Value.Get<FVector2D>();
+
+    // 카메라 회전 기준으로 앞 방향을 찾기
+    const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+
+    const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+    const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+    GetCharacter()->AddMovementInput(ForwardDirection, MovementVector.Y);
+    GetCharacter()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ACC_PlayerController::Look(const FInputActionValue& Value)
+{
+    const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+    AddYawInput(LookAxisVector.X);
+    AddPitchInput(LookAxisVector.Y);
+}
+
+void ACC_PlayerController::Primary()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Primary Action!"));
+}
---
+++ Source/Crash/Public/Player/CC_PlayerController.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CC_PlayerController.generated.h"
+
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class CRASH_API ACC_PlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void SetupInputComponent() override;
+
+private:
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+    TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+
+#pragma region Movement Override
+    UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+    TObjectPtr<UInputAction> JumpAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+    TObjectPtr<UInputAction> LookAction;
+
+
+    void Jump();
+    void StopJumping();
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
+
+#pragma endregion
+
+#pragma region Ability Override
+
+    UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Abilities")
+    TObjectPtr<UInputAction> PrimaryAction;
+
+    void Primary();
+
+#pragma endregion
+
+};
